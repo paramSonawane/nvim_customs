81d43ade684572ed4962962488151815af6e6ef3
paramSonawane <paramsonawane@gmail.com>
2024-01-31 18:15:01 +0530

    add logic to underline the wrong word

--- configs/nvimlint.lua
+++ configs/nvimlint.lua
@@ -10,13 +10,33 @@ return function()
         cmd = 'codespell',
         stdin = false,
         ignore_exitcode = true,
-        parser = require('lint.parser').from_errorformat(
-            '%f:%l:%c:%m',
-            {
-                severity = vim.diagnostic.severity.INFO,
-                source = 'codespell'
-            }
-        )
+        parser = function(output)
+            local efm = '%f:%l:%c:%m'
+            local lines = vim.split(output, '\n')
+            local qflist = vim.fn.getqflist({ efm = efm, lines = lines })
+            local result = {}
+            for _, item in pairs(qflist.items) do
+                if item.valid == 1 then
+                    local message = item.text
+                    local word = message:match('%f[%a](%w+) ==>')
+                    local lnum = math.max(0, item.lnum - 1)
+                    local col = math.max(0, item.col - 1)
+                    local end_lnum = item.end_lnum > 0 and (item.end_lnum - 1) or lnum
+                    local end_col = col + word:len() or col
+                    local diagnostic = {
+                        col = col,
+                        end_lnum = end_lnum,
+                        end_col = end_col,
+                        lnum = lnum,
+                        message = message,
+                        severity = vim.diagnostic.severity.INFO,
+                        source = 'codespell'
+                    }
+                    table.insert(result, diagnostic)
+                end
+            end
+            return result
+        end
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true });
