7d3eb4bdfcd4470ece3e061439d7b985df3dfa30
paramSonawane <paramsonawane@gmail.com>
2024-02-20 19:52:25 +0000

    linting fix

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -3,13 +3,12 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
 
-
 lspconfig.gopls.setup({
-    on_attach=on_attach,
-    capabilities=capabilities,
-    cmd ={"gopls"},
-    filetypes = {"go", "gomod", "gowork", "gotmpl"},
-    root_dir=util.root_pattern("go.work", "go.mod", ".git"),
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { "gopls" },
+    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
     settings = {
         gopls = {
             completeUnimported = true,
