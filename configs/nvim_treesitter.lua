e0fc04009a00c1b059e1ef77d2365a990e632da1
paramSonawane <paramsonawane@gmail.com>
2024-02-28 20:53:34 +0530

    add ensure installed list for nvim treesitter

---
+++ configs/nvim_treesitter.lua
@@ -0,0 +1,14 @@
+return {
+    ensure_installed = {
+        "json",
+        "regex",
+        "lua",
+        "javascript",
+        "typescript",
+        "python",
+        "rust",
+        "html",
+        "markdown",
+        "markdown_inline"
+    }
+}
