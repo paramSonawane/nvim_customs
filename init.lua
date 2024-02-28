11d373bf8f5ed6315693bf92e9a1dd720345c84a
paramSonawane <paramsonawane@gmail.com>
2024-02-28 20:47:26 +0530

    add fold support via treesitter

--- init.lua
+++ init.lua
@@ -1,8 +1,15 @@
-vim.opt.colorcolumn = "80"
-vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.expandtab = true
+local opt = vim.opt
+
+opt.colorcolumn = "80"
+opt.relativenumber = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.expandtab = true
 vim.wrap = true
 vim.macmeta = true
 vim.updatetime = 1000
+
+-- folding
+opt.foldmethod = 'expr'
+opt.foldexpr = 'nvim_treesitter#foldexpr()'
+opt.foldenable = false
