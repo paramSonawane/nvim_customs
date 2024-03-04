6ada2a848cb2e2478a7e53f9ff8d8c4b80212709
paramSonawane <paramsonawane@gmail.com>
2024-03-04 12:27:50 +0530

    main: add nvdash

--- chadrc.lua
+++ chadrc.lua
@@ -17,6 +17,30 @@ M.ui = {
         -- )
         -- end,
     },
+    telescope = {
+        style = "bordered"
+    }, -- borderless / bordered
+    nvdash = {
+        load_on_startup = true,
+        header = {
+            "███╗░░██╗██╗░░░██╗░█████╗░██╗░░██╗░█████╗░██████╗░",
+            "████╗░██║██║░░░██║██╔══██╗██║░░██║██╔══██╗██╔══██╗",
+            "██╔██╗██║╚██╗░██╔╝██║░░╚═╝███████║███████║██║░░██║",
+            "██║╚████║░╚████╔╝░██║░░██╗██╔══██║██╔══██║██║░░██║",
+            "██║░╚███║░░╚██╔╝░░╚█████╔╝██║░░██║██║░░██║██████╔╝",
+            "╚═╝░░╚══╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░",
+        },
+
+        buttons = {
+            { "  Find File", "Spc f f", "Telescope find_files" },
+            { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+            { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+            { "  Bookmarks", "Spc m a", "Telescope marks" },
+            { "  Themes", "Spc t h", "Telescope themes" },
+            { "  Mappings", "Spc c h", "NvCheatsheet" },
+            { "  Quit", "ZZ", ":q!" },
+        }
+    }
 }
 
 M.plugins = 'custom.plugins'
